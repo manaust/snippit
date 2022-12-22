3ef7513c84ac109e5e11c3da5680ab78ad4060d3
manaust <manaustransez@hotmail.com>
2022-12-22 20:08:08 +0100

    :sparkles: Initial commit

---
+++ src/pages/_app.jsx
@@ -0,0 +1,41 @@
+import Head from 'next/head'
+import { Router, useRouter } from 'next/router'
+import { MDXProvider } from '@mdx-js/react'
+
+import { Layout } from '@/components/Layout'
+import * as mdxComponents from '@/components/mdx'
+import { useMobileNavigationStore } from '@/components/MobileNavigation'
+
+import '@/styles/tailwind.css'
+import 'focus-visible'
+
+function onRouteChange() {
+  useMobileNavigationStore.getState().close()
+}
+
+Router.events.on('hashChangeStart', onRouteChange)
+Router.events.on('routeChangeComplete', onRouteChange)
+Router.events.on('routeChangeError', onRouteChange)
+
+export default function App({ Component, pageProps }) {
+  const appName = 'Snippit'
+  let router = useRouter()
+
+  return (
+    <>
+      <Head>
+        {router.pathname === '/' ? (
+          <title>{appName}</title>
+        ) : (
+          <title>{`${pageProps.title} | ${appName}`}</title>
+        )}
+        <meta name="description" content={pageProps.description} />
+      </Head>
+      <MDXProvider components={mdxComponents}>
+        <Layout {...pageProps}>
+          <Component {...pageProps} />
+        </Layout>
+      </MDXProvider>
+    </>
+  )
+}
